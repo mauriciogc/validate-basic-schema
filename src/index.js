9fb33a058430fabacbbbd63547477ce2bc298ced
mauriciogc <mauriciogc@gmail.com>
2020-10-12 12:35:49 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Validator } from "@cfworker/json-schema";
+
+const validator = new Validator({
+	$schema: "http://json-schema.org/draft/2019-09/schema#",
+});
+
+//const result = validator.validate();
+//console.log(result);
